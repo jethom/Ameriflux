f43b2053d2af7b5a00a6fb44a1342ff6c780228b
jthom <jthom@da34f63f-7361-4e55-a2ea-20c0de220f3a>
2014-07-30 21:01:24 +0000

    Updates to batch programs. Moving data into new data structure

--- WLEF/batch/newtrailer.bat
+++ WLEF/batch/newtrailer.bat
@@ -5,24 +5,24 @@ cd C:\Campbellsci\Loggernet
 
 if %time:~0,2% lss 10 (
 if %time:~3,2% lss 30 (
-set dtF=%date:~-4%_%date:~4,2%_%date:~7,2%_0%time:~1,1%_00
+set dtF=%date:~-4%_%date:~4,2%\%date:~7,2%\0%time:~1,1%00
 ) else (
-set dtF=%date:~-4%_%date:~4,2%_%date:~7,2%_0%time:~1,1%_30
+set dtF=%date:~-4%_%date:~4,2%\%date:~7,2%\0%time:~1,1%30
 )
 ) else (
 if %time:~3,2% lss 30 (
-set dtF=%date:~-4%_%date:~4,2%_%date:~7,2%_%time:~0,2%_00
+set dtF=%date:~-4%_%date:~4,2%\%date:~7,2%\%time:~0,2%00
 ) else (
-set dtF=%date:~-4%_%date:~4,2%_%date:~7,2%_%time:~0,2%_30
+set dtF=%date:~-4%_%date:~4,2%\%date:~7,2%\%time:~0,2%30
 )
 )
 
-mkdir C:\Users\Flux\Documents\WLEF\ArchiveData\FluxData\%dtF%\newtrailer
+mkdir C:\Users\Flux\Documents\WLEF\ArchiveData\Data\%dtF%\newtrailer
 dir
 
 ::rename trailer_final_storage_1.dat trailer_final_storage_%dtF%.dat
 
-move newtrailer_*.dat C:\Users\Flux\Documents\WLEF\ArchiveData\FluxData\%dtF%\newtrailer
+move newtrailer_*.dat C:\Users\Flux\Documents\WLEF\ArchiveData\Data\%dtF%\newtrailer
 
 
 ::cd C:\Users\Flux\Documents\WLEF\ArchiveData\FluxData\%dtF%\trailer
