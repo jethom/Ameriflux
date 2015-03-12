0d843f4ce2f2ca1fa72f663f474461453a6a3733
jthom <jthom@da34f63f-7361-4e55-a2ea-20c0de220f3a>
2015-03-12 17:53:53 +0000

    Added Loggernet configuration file to repository.

--- trunk/WillowCreek/scripts/CheckArchiveData.bash
+++ trunk/WillowCreek/scripts/CheckArchiveData.bash
@@ -5,12 +5,12 @@
 # If a screen session is already running with that name, exit, otherwise start
 # a new detached screen session named 'ArchiveData'
 #
-SCRN=$(screen -list | grep ArchiveData | awk '{print $1}')
-workdir=$(eval echo ~)
+SCRN=$(/usr/bin/screen -list | /bin/grep ArchiveData | /usr/bin/awk '{print $1}')
+workdir=$(eval /bin/echo ~)
 pyscriptname="${workdir}/Documents/WillowCreek/scripts/loop2.py"
 if [ -n "$SCRN" ]; then
 echo Screen already running: $SCRN
 else
-screen -S ArchiveData -d -m python "${pyscriptname}"
-echo Started in screen $(screen -list | grep ArchiveData | awk '{print $1}')
+/usr/bin/screen -S ArchiveData -d -m /home/flux/env/bin/python "${pyscriptname}"
+echo Started in screen $(/usr/bin/screen -list | /bin/grep ArchiveData | /usr/bin/awk '{print $1}')
 fi
