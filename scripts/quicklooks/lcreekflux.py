073ab7095bb59cf7302e71d8f71ad5aa971290f2
jthom <jthom@da34f63f-7361-4e55-a2ea-20c0de220f3a>
2015-10-02 16:20:40 +0000

    code to make figures of lost creek calculated fluxes

---
+++ scripts/quicklooks/lcreekflux.py
@@ -0,0 +1,49 @@
+#
+#
+# make daily plots of the lost creek flux calculations
+#
+#
+#-----------------------------------------------------
+import matplotlib
+import sys
+import collections
+from datetime import datetime, timedelta
+import urllib2
+from dataread import flux_read
+import numpy as np
+import pandas as pd
+import xray
+from record2xray import record2xray
+
+plotvars=['cflux', 'qflux', 'tflux', 'ch4flux', 'cstor', 'qstor', 'tstor', 'ch4stor','NEE_CO2', 'LE', 'H', 'NEE_CH4', 'u*']
+runmode = 'TEST'
+if runmode == 'OPER':
+# file directory
+# TEST -------------------------------------------------------------------------------
+    figdir='/Users/jthom/public_html/images/lostcreek/'
+# TEST -------------------------------------------------------------------------------
+else:
+# OPERATIONAL ------------------------------------------------------------------------
+    matplotlib.use('Agg')
+    figdir='/home/jthom/public_html/images/lostcreek/'
+# OPERATIONAL ------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+today=datetime.now()
+tminus5=today-timedelta(5)
+
+TsliceE=today.strftime('%Y-%m-%d')
+TsliceB=tminus5.strftime('%Y-%m-%d')
+
+filename=today.strftime('http://flux.aos.wisc.edu/data/lcreek-raw/%Y/flux/lcreek%Y_flux.txt')
+flptr=urllib2.urlopen(filename)
+
+data=flux_read(flptr)
+
+flptr.close()
+
+datax=record2xray(data)
+for var in plotvars:
+    datax.sel(varname=var, time=slice(TsliceB, TsliceE)).plot(marker='.')
+    plt.savefig(figdir + var + '.png', dpi=100)
+    plt.close()
